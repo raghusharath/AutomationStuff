bd12ab0d687499afd08bd153b6b376465c731c90
raghusharath <raghusharth11.12@gmail.com>
2022-03-27 09:55:44 +0530

    my first commit

---
+++ src/PropertiesLoad.java
@@ -0,0 +1,26 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.System.Logger;
+import java.util.Properties;
+
+
+
+public class PropertiesLoad {
+
+//private static org.apache.log4j.Logger log= LogManager.getLogger(PropertiesLoad.class.getName());	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+			Properties prop=new Properties();
+			FileInputStream fip=new FileInputStream("C:\\Users\\Raghavendra N\\eclipse-workspace\\CoreJava\\data.properties");
+			prop.load(fip);
+			//log.info("log informaration");
+			System.out.println(prop.getProperty("url"));
+			
+			prop.setProperty("name", "ragh");
+			FileOutputStream fos= new FileOutputStream("C:\\Users\\Raghavendra N\\eclipse-workspace\\CoreJava\\data.properties");
+			prop.store(fos, null);
+	}
+
+}
